24f2dc799badd44c0ce86c6136faad49c8538653
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-22 21:05:08 +0600

    Update nagios-ubuntu.sh

--- nagios-ubuntu.sh
+++ nagios-ubuntu.sh
@@ -1,29 +1,46 @@
+##################################
 #Install Nagios Core in Ubuntu 22.04.x
+##################################
 #Reference: 
 #https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/4/en/quickstart-ubuntu.html
 #https://kifarunix.com/install-and-setup-nagios-on-ubuntu-22-04/
 #https://www.virtono.com/community/tutorial-how-to/how-to-install-nagios-on-ubuntu-22-04/#Extract_and_Compile_Nagios_Core
+#https://hostadvice.com/how-to/web-hosting/ubuntu/how-to-install-nagios-on-an-ubuntu/
+#https://www.tecmint.com/install-nagios-core-in-ubuntu-and-debian/
+##################################
+
 
 apt update -y
 
-apt install autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php libgd-dev libssl-dev -y
+apt install nano make apache2-utils build-essential vautoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php libgd-dev libssl-dev -y
 
-wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.4.7.tar.gz
-tar xzf nagios-4.4.7.tar.gz
+wget https://go.nagios.org/get-core/4-5-2/nagios-4.5.2.tar.gz
+tar xzf nagios-4.5.2.tar.gz
+cd nagios-4.5.2.tar.gz
 ls
-cd nagios-4.4.7
 ./configure --with-httpd-conf=/etc/apache2/sites-enabled
 make all
-make install-groups-users
-usermod -aG nagios www-data
 make install
 make install-init
-make install-daemoninit
-make install-config
 make install-commandmode
+systemctl enable nagios.service
+make install-config
 make install-webconf
-a2enmod rewrite cgi
 htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+a2enmod cgi
+systemctl restart apache2
+systemctl start nagios
+systemctl enable nagios
+
+
+make install-groups-users
+usermod -aG nagios www-data
+
+make install-daemoninit
+
+
+
+
 systemctl enable --now nagios
 systemctl restart nagios
 #systemctl status nagios
