04db6bcf2ef980a204632fe6cd8ae5ea1283b970
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-22 21:36:09 +0600

    Update nagios-ubuntu.sh

--- nagios-ubuntu.sh
+++ nagios-ubuntu.sh
@@ -13,7 +13,8 @@
 apt update -y
 
 apt install nano make apache2-utils build-essential vautoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php libgd-dev libssl-dev -y
-
+useradd nagios
+usermod -a -G nagios www-data
 wget https://go.nagios.org/get-core/4-5-2/nagios-4.5.2.tar.gz
 tar xzf nagios-4.5.2.tar.gz
 cd nagios-4.5.2.tar.gz
@@ -29,40 +30,59 @@ make install-webconf
 htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
 a2enmod cgi
 systemctl restart apache2
+systemctl enable apache2
 systemctl start nagios
 systemctl enable nagios
-
-
-make install-groups-users
-usermod -aG nagios www-data
-
-make install-daemoninit
-
-
-
-
-systemctl enable --now nagios
 systemctl restart nagios
-#systemctl status nagios
-systemctl restart apache2
-systemctl enable --now apache2
-#ufw allow Apache
-
+#Access Nagios Dashboard: http://IP-Address/nagios
+systemctl restart nagios
 echo -e "http://Nagios-Server-IP-OR-Hostname/nagios"
+echo -e " Deafult User(nagioadmin) "
+echo -e " Deafult Password(Which You Given When Exec This Shell ) "
 
-#Install Nagios Plugins on Ubuntu 22.04
+#Install Nagios Plugins on Ubuntu 22.04.x
 
-sudo apt update
-sudo apt install nagios-plugins
-cp /usr/lib/nagios/plugins/* /usr/local/nagios/libexec/
+apt install libmcrypt-dev make libssl-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext libldap2-dev smbclient fping libmysqlclient-dev libdbi-dev -y
+wget https://github.com/nagios-plugins/nagios-plugins/releases/download/release-2.4.0/nagios-plugins-2.4.0.tar.gz
+tar xzf nagios-plugins-2.4.0.tar.gz
+cd nagios-plugins-2.4.0
+./tools/setup 
+./configure 
+make
+make install
+ls /usr/local/nagios/libexec/
+#./configure --with-nagios-user=nagios --with-nagios-group=nagios
+systemctl restart nagios.service
+a2enmod ssl 
+a2ensite default-ssl.conf
+systemctl restart apache2
+systemctl restart nagios
 
-sudo /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
-sudo systemctl restart nagios
-sudo systemctl restart apache2
-sudo ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/
-sudo a2enmod cgi rewrite
+#If you receive an error like this one:
+#Edit the config file:
+#nano /usr/local/nagios/etc/nagios.cfg
+#And replace the 1 with a 0 in the check_for_updates line:
+#check_for_updates=0
+#sed -i 's/check_for_updates=1/check_for_updates=0/' /usr/local/nagios/etc/nagios.cfg
 
 sudo systemctl enable nagios
 sudo systemctl enable apache2
-sudo systemctl restart nagios
-sudo systemctl restart apache2
+
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+
+#Configuring Nagios
+#Include the email address where you want to be receiving monitoringn alerts.
+#vi /usr/local/nagios/etc/objects/contacts.cfg
+#it in one line with the following command, replacing “myemail” with your email:
+
+sed -i 's/nagios@localhost/sumonpaul267@gmail.com/' /usr/local/nagios/etc/objects/contacts.cfg
+
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+
+systemctl restart apache2
+systemctl restart nagios
+
+echo -e " Deafult User(nagioadmin) "
+echo -e " Deafult Password(Which You Given When Exec This Shell ) "
+
+
